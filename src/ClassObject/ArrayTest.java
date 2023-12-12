1754fb9cd7409f2c031547edd334f805684c0dd0
asil ahmed <hashimchennadan@gmail.com>
2023-12-12 21:23:22 +0530

    second commit

--- src/ClassObject/ArrayTest.java
+++ src/ClassObject/ArrayTest.java
@@ -5,13 +5,27 @@ import java.util.Arrays;
 public class ArrayTest {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        arr[0]=5;
-        arr[1]=10;
-        arr[2]=15;
+        arr[0] = 5;
+        arr[1] = 10;
+        arr[2] = 15;
         System.out.println(arr); //output starts with a class name
         System.out.println(Arrays.toString(arr)); //values
         System.out.println(arr.getClass().getName()); //to find class name
+
+
+        double[] arr1 = new double['a']; //here, through implicit typecasting char is converted to integer with the help of ascii table
+        arr1[0] = 5;
+        arr1[10] = 10;
+        arr1[2] = 15;
+        System.out.println(arr1); //output starts with a class name
+        System.out.println(Arrays.toString(arr1)); //values
+        System.out.println(arr1.getClass().getName()); //to find class name
+        System.out.println("length of arr1 : " + arr1.length);
     }
 
 
 }
+
+
+
+
