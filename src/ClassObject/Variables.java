025eed130b1c60bfd5f93ebb703b4217803c9675
asil ahmed <hashimchennadan@gmail.com>
2023-12-13 22:45:14 +0530

    fourth

--- src/ClassObject/Variables.java
+++ src/ClassObject/Variables.java
@@ -6,19 +6,20 @@ public class Variables {
         Student s1 = new Student();
         s1.name="Hashim";
         s1.roll=123;
-        s1.schoolName="DRS";
+        Student.schoolName="DRS"; //since schoolName is static it should be addressed with class name since it has only one copy for all objects
 
         Student s2 = new Student();
         s2.name="Qureshi";
         s2.roll=124;
-        s2.schoolName="DRS";
+        Student.schoolName="DRS";
 
         Student s3 = new Student();
         s3.name="Chennadan";
         s3.roll=125;
-        s3.schoolName="DRS";
+        Student.schoolName="DRS";
 
         System.out.println(s1.name);
+        System.out.println(s1);
         System.out.println(s2); //to string method is being overriden and customised in student class.
         System.out.println(s3);
 
